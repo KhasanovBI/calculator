af21914186c404d2a579ef6eb684144a6f108f28
Bulat Khasanov <afti@yandex.ru>
2017-02-05 01:14:59 +0300

    add travis

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from setuptools import find_packages
 setup(
     name='Calculator',
     version='0.0.1',
-    packages=find_packages(),
-    url='',
+    packages=find_packages(exclude=('tests',)),
+    url='https://github.com/KhasanovBI/calculator',
     license='BSD',
     author='bkhasanov',
     author_email='afti@yandex.ru',
